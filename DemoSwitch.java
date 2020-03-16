e2317928002d26509dfb16af716c65834f8d6006
KarishmaJoseph <62091924+KarishmaJoseph@users.noreply.github.com>
2020-03-17 00:48:03 +0530

    Add files via upload

---
+++ DemoSwitch.java
@@ -0,0 +1,15 @@
+class DemoSwitch
+{
+ public static void main(String args[])
+  {
+   int num=10;
+   switch(num){
+   case 10:System.out.println("10");
+   break;
+   case 20:System.out.println("20");
+   break;
+   case 30:System.out.println("30");
+   break;
+   }
+  }
+}
