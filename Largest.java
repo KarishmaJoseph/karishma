c97db868da764a540d81a8f1cf518756302f7468
KarishmaJoseph <62091924+KarishmaJoseph@users.noreply.github.com>
2020-03-17 00:23:30 +0530

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,13 @@
+class Largest
+{
+  public static void main(String args[]){
+     int num1=50, num2=70, num3=60;
+
+      if(num1 >= num2 && num1 >= num3)
+        System.out.println(num1+ "is the largest number");
+      else if(num2 >= num1 && num2 >=num3)
+        System.out.println(num2+ "is the largest number");
+      else
+        System.out.println(num2+ "is the largest number");
+     }
+}
