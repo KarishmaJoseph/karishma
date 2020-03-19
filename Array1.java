e2f4f932e76089c3e0bb1c6502867b9240e20cf7
KarishmaJoseph <62091924+KarishmaJoseph@users.noreply.github.com>
2020-03-20 02:04:17 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,15 @@
+class Array1
+  {
+    public static void main(String args[])
+     { 
+     int arr[]=new int[5];
+     for(int i=0;i<5;i++)
+     {
+      arr[i]=i+1;
+     }
+     for(int i:arr){
+     System.out.println(i);
+     }
+    }
+   }
+
