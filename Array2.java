94b3608ab232488c6564b6e57874e7a5acbe3a11
KarishmaJoseph <62091924+KarishmaJoseph@users.noreply.github.com>
2020-03-20 02:06:55 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Array2
+  {
+    public static void main(String args[])
+     { 
+     Random r=new Random();
+     int arr[]=new int[30];
+     for(int i=0;i<arr.length;i++)
+     {
+      arr[i]=r.nextInt(50);
+     }
+    try
+    {
+     System.out.println(arr[32]);
+    }
+    catch(Exception e)
+    {
+    System.out.println("Maximum index value is : 49");
+    }
+     for(int i:arr){
+     System.out.println(i);
+     }
+    }
+   }
