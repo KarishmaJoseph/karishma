f8087d3c3241257e90a2746d50ae5c9127acc36e
KarishmaJoseph <62091924+KarishmaJoseph@users.noreply.github.com>
2020-03-21 01:15:17 +0530

    Add files via upload

---
+++ EnhancedFor.java
@@ -0,0 +1,18 @@
+class EnhancedFor{
+public static void main(String args[])
+{
+
+   int a[][]={
+               {1,2,3,4},
+               {3,4,5,6},
+               {4,5,1,2}};
+     for(int []i:a)
+            {
+            for(int j:i)
+             {
+              System.out.print(j+" ");
+}
+      System.out.println();
+}
+}
+}
