fbfdc38640d8767d1700ee25f24128eb9d0fdac1
KarishmaJoseph <62091924+KarishmaJoseph@users.noreply.github.com>
2020-03-21 00:58:45 +0530

    Add files via upload

---
+++ Array3.java
@@ -0,0 +1,17 @@
+class Array3 {
+   public static void main(String[] args) {
+
+   int a[][]={
+               {1,2,3,4},
+               {3,4,5,6},
+               {4,5,1,2}};
+     for(int i=0;i<3;i++)
+            {
+            for(int j=0;j<4;j++)
+             {
+              System.out.print(a[i][j]+ " ");
+}
+      System.out.println();
+}
+}
+}
