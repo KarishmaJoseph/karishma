694b9cf7b74548502ed677b8cfcca75cf1b7a1a2
KarishmaJoseph <62091924+KarishmaJoseph@users.noreply.github.com>
2020-03-21 20:19:54 +0530

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,16 @@
+public class Static {
+  public static void main(String[] args)
+        {
+             A2.show();
+        }  
+}
+class A2
+{
+    static int i;
+    public static void show()
+    {
+        System.out.println("how r u");
+        System.out.println(i);
+    }
+}
+
